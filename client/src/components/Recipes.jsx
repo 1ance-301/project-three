6e51ddc7f265b5c31ebfe872e28838b07a917f27
William Wettering <sirwettering@github.com>
2022-01-13 21:14:51 -0500

    added components for various pages. Made basic layout and page works

---
+++ client/src/components/Recipes.jsx
@@ -0,0 +1,28 @@
+
+import React from "react";
+
+function Recipes() {
+  return (
+    <div className="recipes">
+      <div class="container">
+        <div class="row align-items-center my-5">
+          <div class="col-lg-7">
+            <img
+              class="img-fluid rounded mb-4 mb-lg-0"
+              src="http://placehold.it/900x400"
+              alt=""
+            />
+          </div>
+          <div class="col-lg-5">
+            <h1 class="font-weight-light">Recipes</h1>
+            <p>
+              Page to search and save recipes. Also Randomize.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Recipes;
